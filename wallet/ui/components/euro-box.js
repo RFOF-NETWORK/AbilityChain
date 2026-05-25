41a707ad3a46895d9c7f2b2f7091cc39aa67eb32
RFOF-NETWORK <actions@github.com>
2026-05-25 15:25:26 +0000

    Deploy AbilityChain build 6cbaef904305ff75b52f2c21664275a0c6ffd037

--- wallet/ui/components/euro-box.js
+++ wallet/ui/components/euro-box.js
@@ -1,8 +1,13 @@
 export function renderEuroBox(euro) {
+  const formatted = Number(euro).toLocaleString("de-DE", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
+
   return `
-    <div class="euro-box">
+    <div class="euro-box" id="euro-box">
       <h2>Fiat</h2>
-      <p>${euro} €</p>
+      <p class="euro-amount">${formatted} €</p>
     </div>
   `;
 }
