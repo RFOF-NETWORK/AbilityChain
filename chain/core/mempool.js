2a235954a7361674f9fef4bac713ea4e12784a37
rfofspidernet.de (AIDA Special Comitee) = rfof-bitcoin.org - = rfofspidernet.de <176731182+RFOF-NETWORK@users.noreply.github.com>
2026-05-25 19:45:48 +0200

    Update mempool.js

--- chain/core/mempool.js
+++ chain/core/mempool.js
@@ -1,17 +1,39 @@
-// Mempool – unbestätigte Transaktionen
+// Mempool – unbestätigte Transaktionen (Validiert & Sicher)
+
+import { Validators } from "./validators.js";
 
 export class Mempool {
   constructor() {
     this.pool = [];
+    this.txHashes = new Set(); // Dubletten-Schutz
   }
 
+  // Fügt eine TX nur hinzu, wenn sie valide ist und noch nicht existiert
   add(tx) {
+    if (!Validators.tx(tx)) {
+      console.warn("Mempool: Ungültige Transaktion abgelehnt");
+      return false;
+    }
+
+    if (this.txHashes.has(tx.hash)) {
+      return false; // Dublette vermeiden
+    }
+
     this.pool.push(tx);
+    this.txHashes.add(tx.hash);
+    return true;
   }
 
+  // Gibt alle ausstehenden TXs für den Explorer zurück
+  getPendingTransactions() {
+    return [...this.pool];
+  }
+
+  // Leert den Pool für die Block-Erstellung
   drain() {
     const txs = [...this.pool];
     this.pool = [];
+    this.txHashes.clear();
     return txs;
   }
 
