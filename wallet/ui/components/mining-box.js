b0dc057933c2e137ce22769e5f585930b9d699de
rfofspidernet.de (AIDA Special Comitee) = rfof-bitcoin.org - = rfofspidernet.de <176731182+RFOF-NETWORK@users.noreply.github.com>
2026-05-25 17:25:56 +0200

    Update mining-box.js

--- wallet/ui/components/mining-box.js
+++ wallet/ui/components/mining-box.js
@@ -1,9 +1,14 @@
 export function renderMiningBox(xp) {
   return `
-    <div class="mining-box">
+    <div class="mining-box" id="mining-box">
       <h2>XP Mining</h2>
-      <p>XP: ${xp}</p>
-      <button id="mine">Tipp-Mining</button>
+      <p class="xp-amount">XP: ${xp}</p>
+
+      <div class="mining-status">
+        <div id="mining-progress" class="progress-bar"></div>
+      </div>
+
+      <button id="mine">Tipp‑Mining starten</button>
     </div>
   `;
 }
