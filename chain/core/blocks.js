88a6a3051602bede9a8b4eb5278d6f3af592a733
RFOF-NETWORK <actions@github.com>
2026-05-25 18:04:58 +0000

    Deploy AbilityChain build 9bbd4f74866bf9a2d7ac94e94e34e85ba7b42c86

--- chain/core/blocks.js
+++ chain/core/blocks.js
@@ -1,24 +1,27 @@
-// Block-Struktur der AbilityChain (Deterministisch & PZQQET-0 konform)
+// Block-Struktur der AbilityChain (Fraktales Drei-Strom-Modell)
 
 import crypto from "crypto";
 
 export class Block {
-  constructor(index, prevHash, transactions, timestamp = Date.now()) {
+  // streamType: 'TX', 'TIME', 'XP'
+  constructor(index, prevHash, transactions, streamType, fractalRoot, timestamp = Date.now()) {
     this.index = index;
     this.prevHash = prevHash;
-    this.transactions = transactions; // Erwartet Array von TXs
+    this.transactions = transactions;
+    this.streamType = streamType; // Neuer Strom-Indikator
+    this.fractalRoot = fractalRoot; // Genesis-Hash als Anker
     this.timestamp = timestamp;
     this.version = "PZQQET-0";
     this.hash = this.computeHash();
   }
 
-  // Erzeugt einen konsistenten, deterministischen Hash
   computeHash() {
-    // Canonical JSON: Sortierung der Schlüssel verhindert Hash-Varianz
     const canonicalData = JSON.stringify({
       index: this.index,
       prevHash: this.prevHash,
       transactions: this.transactions,
+      streamType: this.streamType,
+      fractalRoot: this.fractalRoot,
       timestamp: this.timestamp,
       version: this.version
     }, Object.keys(this).sort());
@@ -29,10 +32,10 @@ export class Block {
       .digest("hex");
   }
 
-  // Validiert den Block gegen den Vorgänger für den Explorer
   isValid(prevBlock) {
     if (this.index !== prevBlock.index + 1) return false;
     if (this.prevHash !== prevBlock.hash) return false;
+    if (this.fractalRoot !== prevBlock.fractalRoot) return false;
     if (this.hash !== this.computeHash()) return false;
     return true;
   }
